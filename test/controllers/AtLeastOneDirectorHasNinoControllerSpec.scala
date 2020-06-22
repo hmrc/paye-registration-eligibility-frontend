8767b4bad1e715d53a1c569f1698c7b0909027f8
Milly Harrison <64021464+HarrNM@users.noreply.github.com>
2020-06-22 10:43:52 +0100

    SAR-5687: replaced traits with injectable classes

--- test/controllers/AtLeastOneDirectorHasNinoControllerSpec.scala
+++ test/controllers/AtLeastOneDirectorHasNinoControllerSpec.scala
@@ -46,7 +46,7 @@ class AtLeastOneDirectorHasNinoControllerSpec extends ControllerSpecBase with Be
   object Controller extends AtLeastOneDirectorHasNinoController(
     frontendAppConfig,
     mockDataCacheConnector,
-    new FakeAuthAction(frontendAppConfig, messagesControllerComponents),
+    new FakeAuthAction(messagesControllerComponents),
     getEmptyCacheMap,
     new DataRequiredAction(messagesControllerComponents),
     formProvider, messagesControllerComponents
@@ -70,7 +70,7 @@ class AtLeastOneDirectorHasNinoControllerSpec extends ControllerSpecBase with Be
       object Controller extends AtLeastOneDirectorHasNinoController(
         frontendAppConfig,
         mockDataCacheConnector,
-        new FakeAuthAction(frontendAppConfig ,messagesControllerComponents),
+        new FakeAuthAction(messagesControllerComponents),
         getRelevantData,
         new DataRequiredAction(messagesControllerComponents),
         formProvider, messagesControllerComponents
