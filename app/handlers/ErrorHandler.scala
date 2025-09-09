be9ed98d73eddbfcc03ecdfe52b3bb7acf00775a
Prem-Dasari <176310925+Prem-Dasari@users.noreply.github.com>
2025-09-09 11:57:30 +0100

    DL-17160: Updated dependency for jackson-core-vulnerability

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,18 +34,22 @@ package handlers
 
 import config.AppConfig
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.Request
+import play.api.mvc.{Request, RequestHeader}
 import play.twirl.api.Html
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import views.html.templates.error_template
 
 import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
 class ErrorHandler @Inject()(val messagesApi: MessagesApi,
-                             view: error_template
-                            )(implicit appConfig: AppConfig) extends FrontendErrorHandler with I18nSupport {
+                             view: error_template,
+                            implicit val appConfig: AppConfig)
+                            (implicit val ec: ExecutionContext) extends FrontendErrorHandler with I18nSupport {
 
-  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: Request[_]): Html =
-    view(pageTitle, heading, message)
+  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: RequestHeader): Future[Html] = {
+    implicit val req: Request[_] = rh.asInstanceOf[Request[_]]
+    Future.successful(view(pageTitle, heading, message))
+  }
 }
 
